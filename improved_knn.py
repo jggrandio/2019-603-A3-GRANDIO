18f713feae42e8c580b5fd441cebebdba77cf6f5
Javier <javiergrandiogonzalez@gmail.com>
2019-12-07 14:24:10 -0500

    new improvements in penformance

--- improved_knn.py
+++ improved_knn.py
@@ -1,12 +1,10 @@
+#Fastest way until now
 from pyspark import SparkContext, SparkConf
 import numpy as np
 import time
 
-
-conf = SparkConf().setAppName("KNN")
-sc = SparkContext(conf=conf)
-
 start = time.time()
+
 def distances (dt):
     idts = dt[1]
     ts = list(map(float, dt[0][0:11]))
@@ -17,17 +15,15 @@ def distances (dt):
     
     for i in tr:
         if not idts == i[1]:
-            dist = sum((p-q)**2 for p, q in zip(i[0][0:11], ts)) ** .5
-            for j in range(len(x)):
-                if dist < x[j][0]:
-                    for z in range(len(x)-1,j,-1):
-                        x[z]=x[z-1]
-                    x[j]=(dist,i[0][11])
-                    break
+            dist = sum((p-q)*(p-q) for p, q in zip(i[0][0:11], ts))
+            if dist < x[len(x)-1][0]:
+                for j in range(len(x)):
+                    if dist < x[j][0]:
+                        x.insert(j,(dist,i[0][11]))
+                        x.pop()
+                        break
                 
     return (cltr,x)
-
-
 def guess_class(dt):
     rclass = dt[0]
     freq = 0
@@ -50,6 +46,7 @@ def correct(dt):
         return 0
 
 
+sc = SparkContext.getOrCreate()
 ts = sc.textFile("datasets/medium.txt").zipWithUniqueId()\
     .map(lambda line: (line[0].split(','), line[1]))\
     .map(lambda line: (list(map(float, line[0])),line[1]))
@@ -64,4 +61,4 @@ correct = guess_class.map(correct)
 accuracy = correct.mean()
 end = time.time()
 print('The time to run is:', end - start)
-print('accuracy')
+print(accuracy)
